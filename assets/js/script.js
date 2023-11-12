5d650c38b452ab0d287181d83a7c8f78df11228c
Jem212Mac <>
2023-11-12 22:42:43 +0000

    Add second set of cards to car array

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,14 @@ cards = [
     { name: "snowman", image: "snowman.webp" },
     { name: "stocking", image: "xmas_stocking.webp" },
     { name: "tree", image: "xmas_tree.webp" },
+    { name: "elf", image: "elf.webp" },
+    { name: "gingerbread", image: "gingerbread_man.webp" },
+    { name: "rudolph", image: "rudolph.webp" },
+    { name: "santa", image: "santa_claus.webp" },
+    { name: "sleigh", image: "santas_sleigh.webp" },
+    { name: "snowman", image: "snowman.webp" },
+    { name: "stocking", image: "xmas_stocking.webp" },
+    { name: "tree", image: "xmas_tree.webp" },
 ];
 
 shuffleCards();
