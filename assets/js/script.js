dbd571f788e4f35f9e7527d95b0681fc468882b3
Jem212Mac <>
2023-11-12 23:36:41 +0000

    Fix typo's in css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,7 @@ function shuffleCards() {
     let currentIndex = cards.length,
     randomIndex,
     tempValue;
-    while (currentIndex == 0) {
+    while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         tempValue = cards[currentIndex];
@@ -45,13 +45,42 @@ function createCards() {
     for (let card of cards) {
         const cardElement = document.createElement("div");
         cardElement.classList.add("card");
-        cardElement.setAttribute("date-name", card.name);
+        cardElement.setAttribute("data-name", card.name);
         cardElement.innerHTML = `
             <div class="front">
-                <img class="front-image" src={card.image}/>
+                <img class="front-image" src=${card.image}/>
             </div>
             <div class="back"></div>
             `;
             cardArea.appendChild(cardElement);
+            cardElement.addEventListener("click", turnCard);
     }
+}
+
+function turnCard () {
+    if(lockGame) return;
+    if (this === firstCard) return;
+
+    this.classList.add("turned");
+    if(!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockGame = true;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isThereMatch = firstCard.dataset.name === secondCard.dataset.name;
+    isThereMatch ? disableCards() : turnBackCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", turnCard);
+    secondCard.removeEventListener("click", turnCard);
 }
