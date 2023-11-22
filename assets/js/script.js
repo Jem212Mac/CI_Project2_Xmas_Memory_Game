4b4e8b90236812ac778d1cfa7f10a6afa88eb878
Jem212Mac <>
2023-11-22 00:29:05 +0000

    Remove modal code - slowing site down - will revisit later

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,13 +131,3 @@ radioBtns.forEach(radioBtn => {
     radioBtn.addEventListener('change', findSelected);
 });
 findSelected();
-/*
-span.onclick = function () {
-    modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}; */
